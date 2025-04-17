c35183e6020c8005abb0ae56f36dfa5fe056d106
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2025-04-17 16:07:25 +0500

    Initial commit

---
+++ 1-Hello-Js.js
@@ -0,0 +1,10 @@
+//Hello Js M-1
+console.log("Hello JS");
+//Hello JS M-2
+document.write("Hello JS");
+// Hello Js M-3
+document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+
+// Hello Js M-4
+
+document.getElementById("demo").innerText = "Hello World";
