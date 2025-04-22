4d3c65525909cdacd6b27fe62ea262c44b885965
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2025-04-22 16:47:15 +0500

    DataTypes

---
+++ 5-DataTypes.js
@@ -0,0 +1,50 @@
+//DataTypes
+//Number
+let numberCNIC= 212121;
+console.log(typeof numberCNIC);
+
+//float
+
+let   temp = 12.5;
+console.log(typeof temp);
+
+
+let x1 = 34.00;
+console.log(typeof x1);
+
+
+
+//integer
+let int = 999999999999999;
+console.log(typeof int);
+
+//string
+
+let name = "Muqtasid";
+console.log(typeof name);
+
+let carName1 = "Volvo XC60";
+console.log(typeof carName1);
+
+
+//BigInt 
+
+let BgInt = BigInt("123456789012345678901234567890");
+console.log(typeof BgInt)
+
+//Boolean
+
+let bool = 10 == 9;
+
+console.log(typeof bool );
+
+//Undefined
+
+let Company;
+console.log(typeof Company);
+
+//Null
+
+let Nll = null;
+
+console.log(typeof Nll);
