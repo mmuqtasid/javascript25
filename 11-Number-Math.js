ea1d2928976d3606ab490cb0c6bccc9032eb508b
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2025-05-09 12:26:03 +0500

    11-Number-Math.js

--- 11-Number-Math.js
+++ 11-Number-Math.js
@@ -11,3 +11,13 @@ console.log(blance.toFixed(2));
 const OtherNumer = 23.1122;
 console.log(OtherNumer.toPrecision(3))
 
+//Math
+console.log(Math.max(2,4));
+console.log(Math.ceil(4.6));
+console.log(Math.min(3 , 6 , 9 , 0));
+console.log(Math.floor(1,3,44,0.1));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1 );
+
+console.log((Math.floor((Math.random()*10)+1 )));
