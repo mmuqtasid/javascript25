7bc16a5142c026cfe7bc3d1363e1839324dc618e
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2025-05-22 12:19:48 +0500

    init

---
+++ 15-Function-Js.js
@@ -0,0 +1,13 @@
+//Function in Js
+
+function myFunction(a , b){
+    return a + b;
+}
+console.log(myFunction(19 , 10));
+
+function newFunction(p1 , p2 , p3){
+    return p1 + p2 * p3
+}
+
+
+console.log(newFunction(2 , 2 , 2));
